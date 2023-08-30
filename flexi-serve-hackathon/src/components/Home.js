1a5a06a54a4dade081c54400b3bd4eb05648686c
asvipersss <ssshackathon@gmail.com>
2023-08-30 21:40:33 +0000

    task(): dynamically fetching plans

--- flexi-serve-hackathon/src/components/Home.js
+++ flexi-serve-hackathon/src/components/Home.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Accordion from "react-bootstrap/Accordion";
+import Spinner from "react-bootstrap/Spinner";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
@@ -9,93 +10,115 @@ import Col from "react-bootstrap/Col";
 
 const Home = () => {
   const usenavigate = useNavigate();
-  
+  const [isLoading, setIsLoading] = useState(false);
+  const [recommendedPlans, setRecommendedPlans] = useState([]);
+  const [topTrendingPlans, setTopTrendingPlans] = useState([]);
+
   useEffect(() => {
+    loadPlans();
   }, []);
+
+  const loadPlans = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      fetch("http://localhost:8080/plans")
+        .then((res) => {
+          if (!res.ok) {
+            return false;
+          }
+          return res.json();
+        })
+        .then((res) => {
+          console.log(res);
+          setIsLoading(false);
+          setRecommendedPlans(res?.recommended);
+          setTopTrendingPlans(res?.topTrending);
+        });
+    }, 3000);
+  };
+  if (isLoading)
+    return (
+      <div
+        style={{
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <Spinner animation="border" variant="primary" />
+      </div>
+    );
 
   return (
     <div>
       <Accordion defaultActiveKey="0">
         <Accordion.Item eventKey="0">
-          <Accordion.Header>Recommended (3)</Accordion.Header>
+          <Accordion.Header>{`Recommended (${recommendedPlans.length})`}</Accordion.Header>
           <Accordion.Body>
             <Container>
               <Row>
-                <Col>
-                  <Card border="dark" style={{ width: "18rem" }}>
-                    <Card.Header>Featured</Card.Header>
-                    <Card.Body>
-                      <Card.Title>$15</Card.Title>
-                      <Card.Text>Validty: 28 days</Card.Text>
-                      <Card.Text>Data: 1.5 GB/day</Card.Text>
-                      <Button variant="primary">Recharge</Button>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col>
-                  <Card border="dark" style={{ width: "18rem" }}>
-                    <Card.Header>Featured</Card.Header>
-                    <Card.Body>
-                      <Card.Title>$15</Card.Title>
-                      <Card.Text>Validty: 28 days</Card.Text>
-                      <Card.Text>Data: 1.5 GB/day</Card.Text>
-                      <Button variant="primary">Recharge</Button>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col>
-                  <Card border="dark" style={{ width: "18rem" }}>
-                    <Card.Header>Featured</Card.Header>
-                    <Card.Body>
-                      <Card.Title>$15</Card.Title>
-                      <Card.Text>Validty: 28 days</Card.Text>
-                      <Card.Text>Data: 1.5 GB/day</Card.Text>
-                      <Button variant="primary">Recharge</Button>
-                    </Card.Body>
-                  </Card>
-                </Col>
+                {recommendedPlans.map((plan) => (
+                  <Col>
+                    <Card
+                      border="dark"
+                      style={{ width: "18rem" }}
+                      key={plan.id}
+                    >
+                      <Card.Header>Featured</Card.Header>
+                      <Card.Body>
+                        <Card.Title className="text-primary">
+                          {plan.price}
+                        </Card.Title>
+                        <Card.Text>
+                          <strong>Validty:</strong> {plan.validity}
+                        </Card.Text>
+                        <Card.Text>
+                          <strong>Data:</strong> {plan.data}
+                        </Card.Text>
+                        <Button variant="primary">Recharge</Button>
+                        <Button onClick={console.log("clicked")} variant="link">
+                          More details
+                        </Button>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                ))}
               </Row>
             </Container>
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="1">
-          <Accordion.Header>Top Trending Plans (3)</Accordion.Header>
+          <Accordion.Header>{`Top Trending Plans (${topTrendingPlans.length})`}</Accordion.Header>
           <Accordion.Body>
             <Container>
               <Row>
-                <Col>
-                  <Card border="dark" style={{ width: "18rem" }}>
-                    <Card.Header>Featured</Card.Header>
-                    <Card.Body>
-                      <Card.Title>$15</Card.Title>
-                      <Card.Text>Validty: 28 days</Card.Text>
-                      <Card.Text>Data: 1.5 GB/day</Card.Text>
-                      <Button variant="primary">Recharge</Button>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col>
-                  <Card border="dark" style={{ width: "18rem" }}>
-                    <Card.Header>Featured</Card.Header>
-                    <Card.Body>
-                      <Card.Title>$15</Card.Title>
-                      <Card.Text>Validty: 28 days</Card.Text>
-                      <Card.Text>Data: 1.5 GB/day</Card.Text>
-                      <Button variant="primary">Recharge</Button>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col>
-                  <Card border="dark" style={{ width: "18rem" }}>
-                    <Card.Header>Featured</Card.Header>
-                    <Card.Body>
-                      <Card.Title>$15</Card.Title>
-                      <Card.Text>Validty: 28 days</Card.Text>
-                      <Card.Text>Data: 1.5 GB/day</Card.Text>
-                      <Button variant="primary">Recharge</Button>
-                    </Card.Body>
-                  </Card>
-                </Col>
+                {topTrendingPlans.map((plan) => (
+                  <Col>
+                    <Card
+                      border="dark"
+                      style={{ width: "18rem" }}
+                      key={plan.id}
+                    >
+                      <Card.Header>Featured</Card.Header>
+                      <Card.Body>
+                        <Card.Title className="text-primary">
+                          {plan.price}
+                        </Card.Title>
+                        <Card.Text>
+                          <strong>Validty:</strong> {plan.validity}
+                        </Card.Text>
+                        <Card.Text>
+                          <strong>Data:</strong> {plan.data}
+                        </Card.Text>
+                        <Button variant="primary">Recharge</Button>
+                        <Button onClick={console.log("clicked")} variant="link">
+                          More details
+                        </Button>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                ))}
               </Row>
             </Container>
           </Accordion.Body>
